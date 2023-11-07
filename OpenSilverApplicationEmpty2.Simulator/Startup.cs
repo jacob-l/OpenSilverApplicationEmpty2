607cfead0582d17f06befa95e2684bda769f537d
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2023-11-07 11:11:55 +0600

    Add project files.

---
+++ OpenSilverApplicationEmpty2.Simulator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using OpenSilver.Simulator;
+using System;
+
+namespace OpenSilverApplicationEmpty2.Simulator
+{
+    internal static class Startup
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            return SimulatorLauncher.Start(typeof(App));
+        }
+    }
+}
